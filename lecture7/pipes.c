02c578a64ea151879378ee9c9fd71bcdacc8d1b3
ben reed <ben.reed@sjsu.edu>
2023-09-13 17:57:44 -0700

    mods from class

--- lecture7/pipes.c
+++ lecture7/pipes.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
 
 //
 // Created by bcr33d on 9/11/23.
@@ -11,9 +13,30 @@ int main() {
     }
     int i = 4;
     int j = 0;
-    if (write(fds[1], &i, sizeof i) != sizeof i) {
+    if (fork() == 0) {
+        i += 17;
+        printf("child set i to %d\n", i);
+        if (write(fds[1], &i, sizeof i) != sizeof i) {
+            perror("write");
+        }
+        exit(0);
+    }
+    wait(NULL);
+    printf("child finished\n");
+    /*
+    int x = 17;
+    if (write(fds[1], &x, sizeof x) != -1) {
         perror("write");
     }
+     */
+    close(fds[1]);
+    if (read(fds[0], &j, sizeof j) != sizeof j) {
+        perror("read");
+    }
+    printf("i = %d\n", i);
+    printf("j = %d\n", j);
+    printf("doing read a second time\n");
+    j = 99;
     if (read(fds[0], &j, sizeof j) != sizeof j) {
         perror("read");
     }
